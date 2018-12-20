f3fbfaf356429a563dd2957a09ace6593d8d23ca
Finn Nilsen <finnnilsen@Finns-MacBook-Pro.local>
2018-12-19 21:49:15 -0800

    updated server

--- server/directories.js
+++ server/directories.js
@@ -5,5 +5,6 @@ module.exports = function(app) {
     // app.use(express.static(path.join(__dirname, 'components')));
     app.use(express.static(path.join(__dirname, '../public')));
     app.use(express.static(path.join(__dirname, '../public/general')));
-    app.use('/submit',express.static(path.join(__dirname, '../public/app')));
+    app.use('/form',express.static(path.join(__dirname, '../public/app')));
+    app.use('/create_usr',express.static(path.join(__dirname, '../public/admin')));
 }
--- server/routes.js
+++ server/routes.js
@@ -1,8 +1,7 @@
 const path = require('path');
 let hamburger = require('../data_files/base_menu.json')
 
-module.exports = function login(app,sessionChecker,User) {
-    
+module.exports = function(app,sessionChecker,User) {
     app.get('/', sessionChecker, (req, res) => {
         res.redirect('/login');
     });
@@ -35,7 +34,11 @@ module.exports = function login(app,sessionChecker,User) {
                         next()
                     } else {
                         req.session.user = user.dataValues;
-                        res.redirect('/submit');
+                        if(req.session.user.user_type==='admin') {
+                            res.redirect('/create_usr');
+                        } else {
+                            res.redirect('/form');
+                        }
                         console.log('user authenticated');
                         console.log('user type => ',user.dataValues.user_type)
                     }
@@ -69,11 +72,22 @@ module.exports = function login(app,sessionChecker,User) {
 
         });
 
-    app.get('/submit', (req, res) => {
+    app.get('/form', (req, res) => {
             console.log('session => ', req.session.user)
             if (req.session.user && req.cookies.auto_sid) {
                 console.log('session started at submit')
-                res.sendFile(path.join(__dirname,'../public/submit-finn.html'));
+                res.sendFile(__dirname + '../public/submit-finn.html');
+            } else {
+                console.log('error')
+                res.redirect('/login');
+            }
+        })
+
+        app.get('/create_usr', (req, res) => {
+            console.log('session => ', req.session.user)
+            if (req.session.user && req.cookies.auto_sid) {
+                console.log('session started at submit')
+                res.sendFile(__dirname + '../public/create_usr-finn.html');
             } else {
                 console.log('error')
                 res.redirect('/login');
@@ -85,7 +99,7 @@ module.exports = function login(app,sessionChecker,User) {
             console.log('session => ', req.session.user)
             if (req.session.user && req.cookies.auto_sid) {
                 console.log('session started at submit')
-                res.sendFile(path.join(__dirname,'../public/styleguid-finn.html'));
+                res.sendFile(__dirname + '../public/styleguid-finn.html');
             } else {
                 console.log('error')
                 res.redirect('/login');
@@ -93,9 +107,11 @@ module.exports = function login(app,sessionChecker,User) {
         })
 
     app.get('/login_menu', (req,res,next) => {
-        
-        console.log(req.session.user)
-        res.json(hamburger.logedin);
+        if(req.session.user.user_type==='admin') {
+            res.json(hamburger.admin);
+        } else {
+            res.json(hamburger.logedin);
+        }
     })
 
     app.get('/logout', function (req, res, next) {
