2861debf11d3137a85c2124ca939b005c7434d1a
finnnilsen90 <fnilsen1051@gmail.com>
2019-11-24 15:32:59 -0800

    updated directories with contact

--- server/directories.js
+++ server/directories.js
@@ -22,6 +22,7 @@ module.exports = function(app) {
         app.use('/home',express.static(path.join(__dirname, '../public/general')));
         app.use('/resume',express.static(path.join(__dirname, '../public/general')));
         app.use('/project',express.static(path.join(__dirname, '../public/general')));
+        app.use('/contact',express.static(path.join(__dirname, '../public/general')));
         app.use('/form',express.static(path.join(__dirname, '../public/app')));
         app.use('/create_login',express.static(path.join(__dirname, '../public/admin')));
     }
