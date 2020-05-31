56e7747514de16f8d88466944d311b442d67526d
finnnilsen90 <fnilsen1051@gmail.com>
2020-05-30 20:09:49 -0700

    projects

--- server/directories.js
+++ server/directories.js
@@ -7,7 +7,7 @@ module.exports = function(app) {
     app.use('/login',express.static(path.join(__dirname, '../public/general')));
     app.use('/home',express.static(path.join(__dirname, '../public/general')));
     app.use('/resume',express.static(path.join(__dirname, '../public/general')));
-    app.use('/project',express.static(path.join(__dirname, '../public/general')));
+    app.use('/projects',express.static(path.join(__dirname, '../public/general')));
     app.use('/contact',express.static(path.join(__dirname, '../public/general')));
     app.use('/form',express.static(path.join(__dirname, '../public/app')));
     app.use('/create_login',express.static(path.join(__dirname, '../public/admin')));
